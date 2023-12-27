28a3092fed6664280901906bc1b81b7d0837d3ac
dominikstipic <dominikstipic@gmail.com>
2023-12-27 22:19:28 +0100

    tinyurl backbone

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from datetime import datetime
+import sqlalchemy as sa
+from models import write
+
+app = Flask(__name__, template_folder=".")
+
+def log_request():
+    request_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    d = {"request_method": request.method, 
+         "request_date": request_date, 
+         "request_uri": request.url,
+         "ip": request.remote_addr
+         }
+    write(d)
+    return d
+
+@app.route("/memory")
+def memory():
+    log_request()
+    
+
+
+if __name__ == "__main__":
+    print("Starting server!")
+    app.run(host="127.0.0.1", port=8080, debug=True)
+    #serve(app, host="0.0.0.0", port=80)
---
+++ models.py
@@ -0,0 +1,24 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, Session
+import pandas as pd
+from typing import List
+
+CONNECTION_STRING = "postgresql://doadmin:AVNS_4894TZ9zuWd4e4qmh5Y@db-postgresql-nyc3-57581-do-user-13892079-0.c.db.ondigitalocean.com:25060/Mneme?sslmode=require"
+
+class Base(DeclarativeBase): 
+    pass
+
+class Request(Base):
+    __tablename__ = "shorturl"
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    request_method = sa.Column(sa.String)
+    request_date = sa.Column(sa.DateTime)
+    request_uri = sa.Column(sa.DateTime)
+    ip = sa.Column(sa.String)
+
+def write(item: dict) -> dict:
+    r = Request(**item)
+    engine = sa.create_engine(CONNECTION_STRING) 
+    with Session(bind=engine) as session:
+        session.add(r)
+        session.commit()
