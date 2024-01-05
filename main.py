759eb2b481f027ef9dc5bfb5ca46f59e67ad023b
dominikstipic <dominikstipic@gmail.com>
2024-01-05 12:16:37 +0100

    add qr_scan and registration endpoint

--- main.py
+++ main.py
@@ -23,8 +23,20 @@ def memory():
     log_request("memory")
     return send_file('repo/memory.mp4', mimetype='mp4')
 
+@app.route("/user/<id>")
+def qr_scan(id):
+    print(id)
+    return "200"
+
+@app.route("/registration", methods=["POST"])
+def registration():
+    user_data = request.json
+    print(user_data)
+    return "200"
+
+
 
 if __name__ == "__main__":
     print("Starting server!")
-    app.run(host="127.0.0.1", port=8080, debug=True)
-    #serve(app, host="0.0.0.0", port=80)
+    #app.run(host="127.0.0.1", port=8080, debug=True)
+    serve(app, host="0.0.0.0", port=80)
