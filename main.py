273a460ded85926059a0728e80d4d2ff4772ef3b
dominikstipic <dominikstipic@gmail.com>
2024-07-18 13:44:54 +0200

    intel

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ from flask import Flask, request, send_file, render_template
 from datetime import datetime
 from waitress import serve
 import logging
-from utils import get_ip_address
+from werkzeug.routing import BaseConverter
 
 app = Flask(__name__, template_folder=".")
 logger = logging.getLogger("doms")
 logging.basicConfig(filename="example.log", level=logging.INFO)
 logging.getLogger('werkzeug').disabled = True
 
-def log_request(resource_name):
-    ip = get_ip_address()
-    if ip == request.remote_addr:
-        entity = "me"
-    else:
-        entity = "other"
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
+#######################################
+
+def log_request(resource_name, entity_name):
     request_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     d = {"resource_name": resource_name,
          "request_method": request.method, 
          "request_date": request_date, 
          "request_uri": request.url,
          "ip": request.remote_addr,
-         "entity": entity}
+         "entity": entity_name}
     logger = logging.getLogger("doms")
     logger.info("{0}".format(d))
 
@@ -29,15 +33,16 @@ def log_request(resource_name):
 def index():
     return render_template('htmls/index.html')
 
-
-@app.route("/is")
-def iron_standard():
-    log_request("is")
+@app.route('/is/<regex("[a-zA-Z0-9_]+"):entity>', methods=['GET'])
+def iron_standard(entity):
+    print("Product")
+    log_request("is", entity)
     return send_file('repo/is.png', mimetype='png')
 
-@app.route("/grb")
-def grb():
-    log_request("grb")
+@app.route('/feud/<regex("[a-zA-Z0-9_]+"):entity>', methods=['GET'])
+def grb(entity):
+    print("Feudal")
+    log_request("feud", entity)
     return send_file('repo/grb.png', mimetype='png')
 
 @app.route("/memory")
@@ -55,8 +60,6 @@ def registration():
     user_data = request.json
     print(user_data)
     return "200"
-
-
 
 if __name__ == "__main__":
     print("Starting server!")
